b7f75cec0104a1bbf6bd7e2b73d092810452970c
Andy Smith <ansmith@redhat.com>
2019-02-07 15:05:51 -0500

    use dns to query peer pod ips

--- qpid-dispatch/run_qdr.sh
+++ qpid-dispatch/run_qdr.sh
@@ -1,6 +1,46 @@
-#!/bin/sh
+#!/bin/bash
 
 export HOSTNAME_IP_ADDRESS=$(hostname -i)
 
-DOLLAR='$' envsubst < /etc/qpid-dispatch/qdrouterd.conf.template > /tmp/qdrouterd.conf
-exec /sbin/qdrouterd --conf /tmp/qdrouterd.conf
+CONFIG_FILE=/tmp/qdrouterd.conf
+TEMPLATE_FILE=/etc/qpid-dispatch/qdrouterd.conf.template
+
+IPS=()
+COUNT=0
+ALL_IPS="false"
+
+while [ "$ALL_IPS" != "true" ]; do
+    while read line
+    do
+        IPS+=("$line")
+        let COUNT=COUNT+1
+    done < <(/usr/bin/dig "$APPLICATION_NAME"-headless."$POD_NAMESPACE".svc.cluster.local +short)
+
+    if [ $COUNT -eq "$POD_COUNT" ]; then
+        ALL_IPS="true"
+    else
+        sleep 1
+        IPS=()
+        COUNT=0
+    fi
+done
+
+if [ -f $TEMPLATE_FILE ]; then
+    DOLLAR='$' envsubst < $TEMPLATE_FILE > $CONFIG_FILE
+    ARGS="-c $CONFIG_FILE"
+fi
+
+for item in "${IPS[@]}"; do
+    if [ "$item" \< "$HOSTNAME_IP_ADDRESS" ]; then
+        cat <<EOF >> $CONFIG_FILE
+    connector {
+        name: $APPLICATION_NAME-$item
+        host: ${item}
+        port: 55672
+        role: inter-router
+    }
+EOF
+    fi
+done
+
+exec /sbin/qdrouterd $ARGS
